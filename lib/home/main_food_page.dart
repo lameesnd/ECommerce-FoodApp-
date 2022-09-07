af5cdb032afb0339ac213a34ad343211b55e17b1
lameesnd <lamees.naseraldean@gmail.com>
2022-09-07 21:34:18 +0300

    initial commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:ecomm_food_app/utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [Text('country'), Text('city')],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.mainColor),
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
